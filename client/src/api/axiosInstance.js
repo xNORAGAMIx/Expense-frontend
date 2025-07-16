477235ce60ac08a3c998726e7dd31fc2ec2ecc7e
Manas Das <noragami0021@gmail.com>
2025-07-16 10:47:48 +0530

    prod api testing

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,7 +1,10 @@
+//aws-url:https://219ac7580940.ngrok-free.app
+//base: "http://localhost:8080/api/v1"
+
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080/api/v1",
+  baseURL: "https://219ac7580940.ngrok-free.app/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
